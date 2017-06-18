f1652e3c66ab7973cb6b1b6ef7818806fd25c404
hideokamoto <hide.okamoto@digitalcube.jp>
2017-06-19 01:54:31 +0900

    add link

--- src/components/globnav/index.js
+++ src/components/globnav/index.js
@@ -11,8 +11,20 @@ class GlobNav extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/en">English</Link></li>
-            <li><a href="">WordPress.org</a></li>
-            <li><a href="">GitHub</a></li>
+            <li>
+              <a
+                href="https://profiles.wordpress.org/hideokamoto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >WordPress.org</a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/hideokamoto/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >GitHub</a>
+            </li>
           </ul>
         </nav>
       </div>
