a18f52c5270e514d5a4c7a1a47655f3b122629ed
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-08 16:43:36 +0900

    init logrocket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-snapshot'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
+import LogRocket from 'logrocket'
+LogRocket.init('jlpbmj/wp-kyoto')
 
 render(<App />, document.getElementById('root'))
 registerServiceWorker()
